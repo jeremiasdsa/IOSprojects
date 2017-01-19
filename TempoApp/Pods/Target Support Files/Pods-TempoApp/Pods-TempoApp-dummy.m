b39c3a4da3f481bf000c25e9f389127bd4632506
jeremias araujo <jeremiasaraujo@jeremiass-iMac.local>
2017-01-19 10:31:27 -0300

    #POD file configured

---
+++ TempoApp/Pods/Target Support Files/Pods-TempoApp/Pods-TempoApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TempoApp : NSObject
+@end
+@implementation PodsDummy_Pods_TempoApp
+@end
---
+++ TempoApp/TempoApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TempoApp
+//
+//  Created by jeremias araujo on 19/01/17.
+//  Copyright © 2017 jeremias araujo. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
